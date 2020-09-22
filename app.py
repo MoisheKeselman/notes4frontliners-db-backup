56c69084b61c017d68d940abb51a66254d6fa281
Moishe Keselman <shimoshik@gmail.com>
2020-09-22 11:00:53 -0700

    changed listening port of app

--- app.py
+++ app.py
@@ -72,5 +72,6 @@ if __name__ == '__main__':
 
    if not os.path.exists(FOLDER_NAME):
       os.mkdir(FOLDER_NAME)
-
-   app.run()
+      
+   port = int(os.environ.get('PORT', 5000))
+   app.run(host='0.0.0.0', port=port)
